eed9e4a686ad2831cbaebafd87c7bb52e123a514
TimAkgayev <worldwidedev@aol.com>
2017-01-27 00:43:51 -0500

    First commit, basic infrastracture in place

---
+++ TestUniWindowsApp2/TestUniWindowsApp2.WindowsPhone/BalancePage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace TestUniWindowsApp2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BalancePage : Page
+    {
+        public BalancePage()
+        {
+            this.InitializeComponent();
+            this._row = new BudgetRow(1, 5, 2017, 4, 54);
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void AddBalanceRow()
+        {
+       
+            BlanceGrid.Children.Add(Row.ExpenseTitle);
+            BlanceGrid.Children.Add(Row.ExpenseAmount);
+            //BlanceGrid.Children.Add(Row.ExpenseDate.ToString());
+            BlanceGrid.Children.Add(Row.SubmitButton);
+            BlanceGrid.Children.Add(Row.DeleteButton);
+        }
+
+        public SolidColorBrush Brush1 = new SolidColorBrush(Colors.Red);
+        private BudgetRow _row;
+        public BudgetRow Row { get { return this._row; } set { this._row = value; }
+    }
+
+ 
+}
---
+++ TestUniWindowsApp2/TestUniWindowsApp2.WindowsPhone/BudgetRow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace TestUniWindowsApp2
+{
+    public class BudgetRow
+    {
+        public BudgetRow(int month, int day, int year, int hour, int minutes)
+        {
+            ExpenseDate = new DateTime(year, month, day, hour, minutes, 0);
+            SubmitButton.Content = "ok";
+            SubmitButton.Width = 20;
+            DeleteButton.Content = "x";
+            DeleteButton.Width = 20;
+
+        }
+
+        public string OneString() { return "We are bound! "; }
+
+        public TextBox ExpenseTitle { get; set; }
+        public TextBox ExpenseAmount { get; set; }
+        public DateTime ExpenseDate { get; set; }
+
+        public Button SubmitButton { get; set; }
+        public Button DeleteButton { get; set; }
+    }
+}
